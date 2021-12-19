e7f4209cd56f2910160cc3e924b545847c79489b
Evgeniy9000 <disabled16@mai.ru>
2021-12-19 13:55:57 +0300

    js

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,9 +1,10 @@
-const getData = () => {
+const getData = (str) => {
     return fetch(
-        "https://test-77071-default-rtdb.firebaseio.com/goods.json"
-    ).then((response) => {
-        return response.json();
-    });
+        `https://test-77071-default-rtdb.firebaseio.com/goods.json?${str ? `search=${str}` : ''}`
+    )
+        .then((response) => {
+            return response.json();
+        });
 };
 
 export default getData;
